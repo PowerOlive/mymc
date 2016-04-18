98c60ccbad411be4b5bdb54779372b5dd9b2cd86
bigmother <xuzhemin@xuzhemindeMacBook-Pro.local>
2016-04-18 22:55:31 +0800

    firstCommit
    yes

---
+++ xzm_scripts/CubeCreate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CubeCreate : MonoBehaviour {
+
+	public GameObject cubePrefab;
+	// Use this for initialization
+	void Start () {
+		Vector3 pos = transform.position;
+		int max = 100;
+		for (int i = 0; i < max; i++) {
+			for (int j = 0; j < max; j++) {
+				Vector3 createPos = new Vector3 (pos.x-max/2+i,1,pos.z-max/2+j);
+				Instantiate (cubePrefab, createPos, transform.rotation);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
