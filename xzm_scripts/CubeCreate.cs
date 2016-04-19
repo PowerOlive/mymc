f0caea4d032fa52eb3a7e860f5861795066b6324
bigmother <xuzhemin@xuzhemindeMacBook-Pro.local>
2016-04-19 22:58:17 +0800

    4-19
    1

--- xzm_scripts/CubeCreate.cs
+++ xzm_scripts/CubeCreate.cs
@@ -7,9 +7,10 @@ public class CubeCreate : MonoBehaviour {
 	void Start () {
 		Vector3 pos = transform.position;
 		int max = 100;
+		int maxHeight = 100;
 		for (int i = 0; i < max; i++) {
 			for (int j = 0; j < max; j++) {
-				Vector3 createPos = new Vector3 (pos.x-max/2+i,1,pos.z-max/2+j);
+				Vector3 createPos = new Vector3 (pos.x-max/2+i,Mathf.RoundToInt(Mathf.PerlinNoise (i/(float)max, j/(float)max)*maxHeight),pos.z-max/2+j);
 				Instantiate (cubePrefab, createPos, transform.rotation);
 			}
 		}
